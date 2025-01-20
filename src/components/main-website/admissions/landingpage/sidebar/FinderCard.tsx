3cd8855dd7cf5cb776a655ffde96f9917398d46e
maheshkumawat274 <maheshkumar006376@gmail.com>
2025-01-20 13:47:23 +0530

    new onw

--- src/components/main-website/admissions/landingpage/sidebar/FinderCard.tsx
+++ src/components/main-website/admissions/landingpage/sidebar/FinderCard.tsx
@@ -1,4 +1,6 @@
 import { FaLaptopCode, FaMicroscope, FaAtom, FaMoneyCheckAlt } from "react-icons/fa";
+//  import { Link } from "react-router-dom";
+// import React from 'react';
 
 const FinderCard = () => {
   return (
